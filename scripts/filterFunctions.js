5bf57fd30f40925a18f04dda387295552dade098
Homero H <homero.herrero.92@gmail.com>
2023-03-13 16:35:39 -0300

    add filter by category feature

--- scripts/filterFunctions.js
+++ scripts/filterFunctions.js
@@ -2,6 +2,12 @@ export function search(events,input){
     return events.filter(event => event.name.toLowerCase().includes(input.toLowerCase())||event.description.toLowerCase().includes(input.toLowerCase()));
 }
 
-export function filterByCategories(events,cat){
-    return events.filter(event => event.category.split(" ")[0] == cat);
+export function filterByCategories(events,cats,cat){
+    let auxCat = cat.split(" ")[0];
+    if(cats.indexOf(auxCat) == -1){
+        cats.push(auxCat)
+    }else{
+        cats.splice(cats.indexOf(auxCat),1)
+    }
+    return cats.length == 0 ? [...events] : [...events.filter(event => cats.indexOf(event.category.split(" ")[0]) !== -1)]
 }
