e25f616e87a01fbf549e6d3c3c64603507427c7c
HomeroHerrero4r6 <homero.herrero.92@gmail.com>
2023-03-22 13:56:18 -0300

    styles

--- scripts/statsFunctions.js
+++ scripts/statsFunctions.js
@@ -15,17 +15,17 @@ export function allEvents(events){
     for(let i=0;i<3;i++){
         const tr = document.createElement("tr");
         tr.innerHTML=`
-            <th scope="row" colspan="2">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <img class="card-img-top cover" style="min-height:100%" src="${highest[i].image}" alt="Card image cap">
+            <th scope="row" colspan="2" style="height:30vh">
+                <div class="container-fluid bg-info h-100">
+                    <div class="row h-100 w-100">
+                        <div class="col h-100 w-50">
+                            <img class="card-img-top cover h-100 w-100" src="${highest[i].image}" alt="Card image cap">
                         </div>
                         <div class="col">
-                            <div class="col">
+                            <div class="row">
                                 <h4 class="card-title text-info text-center">${highest[i].name}</h4>
                             </div>
-                            <div class="col">
+                            <div class="row">
                                 <h5 class="text-info text-center">Audience: <span class="text-danger">${!highest[i].assistance ?highest[i].estimate:highest[i].assistance}</span></h5>
                             </div>
                         </div>
@@ -60,7 +60,7 @@ export function allEvents(events){
                             <h4 class="card-title text-info text-center">${capacity[i].name}</h4>
                         </div>
                         <div class="col">
-                            <h5 class="text-info text-center">Audience: <span class="text-success">${capacity[i].capacity}</span></h5>
+                            <h5 class="text-info text-center">Capacity: <span class="text-success">${capacity[i].capacity}</span></h5>
                         </div>
                     </div>
                 </div>
